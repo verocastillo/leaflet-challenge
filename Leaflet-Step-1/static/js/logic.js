dc67287c2a6a87ee84126a9b8ffe0c74412c18a0
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-07-17 12:34:42 -0500

    Markers work. Colors don't.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -15,19 +15,47 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 // Data source URL
-var eqURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Create query and call function
+d3.json(queryUrl).then(function(data) {
+    getMap(data.features);
+  });
+
+// Function for markers
+function getMap(data) {
+    // Create arrays
+    var earthquakeinfo = [];
+    for (var i = 0; i < data.length; i++) {
+        earthquakeinfo.push({
+        longitude: data[i].geometry.coordinates[0],
+        latitude: data[i].geometry.coordinates[1],
+        magnitude: data[i].properties.mag,
+    })}
+    // Check dictionary
+    console.log(earthquakeinfo)
+    console.log(typeof earthquakeinfo[0].magnitude)
+    // Add markers
+    for (var i = 0; i < earthquakeinfo.length; i++) {
+        L.circleMarker([earthquakeinfo[i].latitude, earthquakeinfo[i].longitude], {
+            color: "black",
+            fillColor: getColor(earthquakeinfo[0].magitude),
+            fillOpacity: 0.8,
+            radius: earthquakeinfo[0].magnitude * 2.5
+        }).addTo(myMap);
+      }
+      return false;
+
+}
 
 // Color function for markers
-function getColor(x) {
-    if (x < 1) { return "Maroon";}
-    if (x < 2.5) {return "Sienna"}
-    if (x < 4) {return "OrangeRed"}
-    if (x < 5.5) {return "Orange"}
-    if (x < 7) {return "Gold"}
+function getColor(magnitude) {
+    if (magnitude < 1.2) {return "maroon";}
+    if (magnitude < 2.4) {return "brickred"}
+    if (magnitude < 3.6) {return "orangered"}
+    if (magnitude < 4.8) {return "orange"}
+    if (magnitude < 6.0) {return "old"}
 };
 
-// Size function for markers
-function mSize(x) {
-    return x * 3 ;
-};
+
 
